cbbdd79d8ddfa29f11a4dac600ed0adc678beb73
Fatma Tandogan Yildiz <fatmatandogan609@gmail.com>
2023-06-01 10:21:07 +0100

    Controller-Viewlar Eklendi.

--- App.Web.Mvc/Controllers/HomeController.cs
+++ App.Web.Mvc/Controllers/HomeController.cs
@@ -17,16 +17,6 @@ namespace App.Web.Mvc.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        
     }
 }
