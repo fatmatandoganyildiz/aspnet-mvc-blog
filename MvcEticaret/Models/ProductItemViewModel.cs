3081e0c5a39affde6b7aabd4113be47a7dd326df
Fatma Tandogan Yildiz <fatmatandogan609@gmail.com>
2023-06-22 23:12:23 +0100

    Task-7

---
+++ MvcEticaret/Models/ProductItemViewModel.cs
@@ -0,0 +1,15 @@
+﻿
+namespace MvcEticaret.Models
+{
+    public class ProductItemViewModel
+    {
+        public string title { get; set; }
+        public int starCount { get; set; }
+        public decimal oldPrice { get; set; }
+        public decimal price { get; set; }
+        public string imageUrl { get; set; }
+        public bool isSale { get; set; }
+
+    }
+
+}
---
+++ MvcEticaret/ViewComponents/ProductItemViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcEticaret.Models;
+using System.Collections.Generic;
+
+namespace MvcEticaret.ViewComponents
+{
+    public class ProductItemViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string title, string imageUrl, int starCount, bool isSale, decimal oldPrice, decimal price)
+        {
+            var product = new ProductItemViewModel()
+            {
+                title = title,
+                price = price,
+                imageUrl = imageUrl,
+                isSale = isSale,
+                oldPrice = oldPrice,
+                starCount = starCount
+            };
+
+            return View(product);
+        }
+    }
+}
